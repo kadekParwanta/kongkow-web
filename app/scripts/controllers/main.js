9478cb06b0217847688ff4061d2cc125b7d02f80
kadekParwanta <kadek.parwanta@gmail.com>
2016-04-21 13:17:25 +0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kongkowWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kongkowWebApp
+ */
+angular.module('kongkowWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ web.js
@@ -0,0 +1,11 @@
+'use strict';
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(port);
+console.log("Express server listening on port " + port);
